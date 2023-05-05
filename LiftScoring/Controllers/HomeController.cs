7db47bd6ce2d8d397b69983d10f01433782c9bbb
PhoebeHoku <95447747+PhoebeHoku@users.noreply.github.com>
2023-05-05 17:16:10 -0400

    Add project files.

---
+++ LiftScoring/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using LiftScoring.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LiftScoring.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var lifter = new LifterModel
+            {
+                Name = "Lixa",
+                isFemale = true,
+                DeadLift = 125,
+                SquatLift = 75,
+                BenchLift = 50,
+                CurlLift = 50,
+                OverheadPressLift = 50,
+                RowLift = 65
+            };
+            return View(lifter);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LiftScoring/Models/LifterModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LiftScoring.Models;
+public class LifterModel
+{
+    public string Name { get; set; }
+    public bool isFemale { get; set; }
+    public double DeadLift { get; set; }
+    public double SquatLift { get; set; }
+    public double BenchLift { get; set; }
+    public double OverheadPressLift { get; set; }
+    public double CurlLift { get; set; }
+    public double RowLift { get; set; }
+
+}

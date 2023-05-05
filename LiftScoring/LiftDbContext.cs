2d22b1563c38f252083ef7195fe65d3dc83618fb
PhoebeHoku <95447747+PhoebeHoku@users.noreply.github.com>
2023-05-05 18:57:24 -0400

    7pm

--- LiftScoring/LiftDbContext.cs
+++ LiftScoring/LiftDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 namespace LiftScoring;
 public class LiftDbContext : DbContext
 {
-    public LiftDbContext() : base("name = LiftDbContext") {}
+    //public LiftDbContext() : base("name = LiftDbContext") {}
     public virtual DbSet<LifterModel> Lifters { get; set; }
     public virtual DbSet<LiftSession> Sessions { get; set; }
     public virtual DbSet<LiftType> Types { get; set; }
